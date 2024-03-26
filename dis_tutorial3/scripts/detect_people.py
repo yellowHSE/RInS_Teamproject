4926df31e0a37c0b0de126ba164d41d34b3e2aaa
yellowHSE <74814515+yellowHSE@users.noreply.github.com>
2024-03-26 18:08:44 +0100

    Add the code to transform the map coorrdinate

--- dis_tutorial3/scripts/detect_people.py
+++ dis_tutorial3/scripts/detect_people.py
@@ -15,6 +15,12 @@ import numpy as np
 import math
 
 from ultralytics import YOLO
+import tf2_geometry_msgs as tfg
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+
+from geometry_msgs.msg import PointStamped
 
 #from robot_commander import robot_commander
 
@@ -24,15 +30,15 @@ from ultralytics import YOLO
 
 #rc = robot_commander()
 
-def distance(p1, p2):
-	return math.sqrt((float(p1[0])-float(p2[0]))**2 + (float(p1[1]) - float(p2[1]))**2)
+#def distance(p1, p2):
+	#return math.sqrt((float(p1[0])-float(p2[0]))**2 + (float(p1[1]) - float(p2[1]))**2)
 
-def point_in_area(point, array, threshold):
-	for p in array:
-		if distance(point, p) <= threshold:
-			return True
+#def point_in_area(point, array, threshold):
+	#for p in array:
+		#if distance(point, p) <= threshold:
+			#return True
 
-	return False
+	#return False
 
 class detect_faces(Node):
 
@@ -63,6 +69,13 @@ class detect_faces(Node):
 		self.model = YOLO("yolov8n.pt")
 
 		self.faces = []
+
+		# TF2 Buffer and Listener for transformations
+		self.tf_buffer = Buffer()
+		self.tf_listener = TransformListener(self.tf_buffer, self)
+
+		# Previously detected face points
+		self.prev_face_points = []
 
 		self.get_logger().info(f"Node has been initialized! Will publish face markers to {marker_topic}.")
 
@@ -107,7 +120,7 @@ class detect_faces(Node):
 			if key==27:
 				print("exiting")
 				exit()
-			
+
 		except CvBridgeError as e:
 			print(e)
 
@@ -121,50 +134,72 @@ class detect_faces(Node):
 
 		# iterate over face coordinates
 		for x,y in self.faces:
+			try:
+				# Get transform from "/base_link" to "/map" frame
+				trans = self.tf_buffer.lookup_transform("map", "base_link", rclpy.time.Time(), rclpy.duration.Duration(seconds=0.1))
 
-			# get 3-channel representation of the poitn cloud in numpy format
-			a = pc2.read_points_numpy(data, field_names= ("x", "y", "z"))
-			a = a.reshape((height,width,3))
+				# get 3-channel representation of the poitn cloud in numpy format
+				a = pc2.read_points_numpy(data, field_names= ("x", "y", "z"))
+				a = a.reshape((height,width,3))
+				# read center coordinates
+				d = a[y,x,:]
 
-			# read center coordinates
-			d = a[y,x,:]
+				# Convert face point from robot frame to map frame
+				face_point_robot_frame = PointStamped()
+				face_point_robot_frame.header = data.header
+				face_point_robot_frame.point.x = float(d[0])
+				face_point_robot_frame.point.y = float(d[1])
+				face_point_robot_frame.point.z = float(d[2])
 
+				face_point_map_stamped = tfg.do_transform_point(face_point_robot_frame, trans)
 
-			print(d)
-
-			if not point_in_area(d, self.saved_markers, 5):
-				self.saved_markers.append(d)
-
-			# create marker
-			marker = Marker()
-
-			marker.header.frame_id = "/base_link"
-			marker.header.stamp = data.header.stamp
-
-			marker.type = 2
-			marker.id = 0
-
-			# Set the scale of the marker
-			scale = 0.1
-			marker.scale.x = scale
-			marker.scale.y = scale
-			marker.scale.z = scale
-
-			# Set the color
-			marker.color.r = 1.0
-			marker.color.g = 1.0
-			marker.color.b = 1.0
-			marker.color.a = 1.0
-
-			# Set the pose of the marker
-			marker.pose.position.x = float(d[0])
-			marker.pose.position.y = float(d[1])
-			marker.pose.position.z = float(d[2])
-
-			self.marker_pub.publish(marker)
-			print(self.saved_markers)
+				# Extract transformed face point
+				face_point_map = face_point_map_stamped.point
 
 
+				# If current face point is same as any of the previous face points, skip publishing marker
+				if any(np.allclose([face_point_map.x, face_point_map.y, face_point_map.z], [prev_face_point.x, prev_face_point.y, prev_face_point.z]) for prev_face_point in self.prev_face_points):
+ 					continue
+
+ 				# Add current face point to the list of previous face points
+				self.prev_face_points.append(face_point_map)
+
+				if len(self.prev_face_points) == 3:
+					self.get_logger().info("ROBOT STOP")
+					self.get_logger().info("Prev face points:")
+					for point in self.prev_face_points:
+						self.get_logger().info(f"({self.prev_face_points})")
+						rclpy.shutdown()
+						return
+
+				# create marker
+				marker = Marker()
+				marker.header.frame_id = "/map"
+				marker.header.stamp = data.header.stamp
+
+				marker.type = Marker.SPHERE
+				marker.id = len(self.prev_face_points) - 1  # Assign unique ID for each marker
+
+				# Set the scale of the marker
+				scale = 0.1
+				marker.scale.x = scale
+				marker.scale.y = scale
+				marker.scale.z = scale
+
+				# Set the color
+				marker.color.r = 1.0
+				marker.color.g = 1.0
+				marker.color.b = 1.0
+				marker.color.a = 1.0
+
+				# Set the pose of the marker
+				marker.pose.position.x = face_point_map.x
+				marker.pose.position.y = face_point_map.y
+				marker.pose.position.z = face_point_map.z
+
+				self.marker_pub.publish(marker)
+			except TransformException as e:
+				self.get_logger().error(f"Transform exception: {e}")
 
 def main():
 	print('Face detection node starting.')
