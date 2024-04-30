19d5d89b76c3f1f16ee8a3260eeb19574cbb9ba9
Songeun Hong <74814515+yellowHSE@users.noreply.github.com>
2024-04-30 18:52:26 +0200

    Update robot_commander.py

--- dis_tutorial3-main/scripts/robot_commander.py
+++ dis_tutorial3-main/scripts/robot_commander.py
@@ -23,6 +23,7 @@ from geometry_msgs.msg import Quaternion, PoseStamped, PoseWithCovarianceStamped
 from lifecycle_msgs.srv import GetState
 from nav2_msgs.action import Spin, NavigateToPose
 from turtle_tf2_py.turtle_tf2_broadcaster import quaternion_from_euler
+from visualization_msgs.msg import Marker
 
 from irobot_create_msgs.action import Dock, Undock
 from irobot_create_msgs.msg import DockStatus
@@ -34,6 +35,12 @@ from rclpy.node import Node
 from rclpy.qos import QoSDurabilityPolicy, QoSHistoryPolicy
 from rclpy.qos import QoSProfile, QoSReliabilityPolicy
 from rclpy.qos import qos_profile_sensor_data
+
+from geometry_msgs.msg import PointStamped
+
+from gtts import gTTS
+import pygame
+from tempfile import TemporaryFile
 
 
 class TaskResult(Enum):
@@ -52,9 +59,11 @@ class RobotCommander(Node):
 
     def __init__(self, node_name='robot_commander', namespace=''):
         super().__init__(node_name=node_name, namespace=namespace)
-        
+
         self.pose_frame_id = 'map'
-        
+        self.face_coordinate_msg = None
+        self.detected_face_num = 0
+
         # Flags and helper variables
         self.goal_handle = None
         self.result_future = None
@@ -62,23 +71,26 @@ class RobotCommander(Node):
         self.status = None
         self.initial_pose_received = False
         self.is_docked = None
+        self.face_coordinate_received = False
 
         # ROS2 subscribers
         self.create_subscription(DockStatus,
                                  'dock_status',
                                  self._dockCallback,
                                  qos_profile_sensor_data)
-        
+
         self.localization_pose_sub = self.create_subscription(PoseWithCovarianceStamped,
                                                               'amcl_pose',
                                                               self._amclPoseCallback,
                                                               amcl_pose_qos)
-        
+
+        self.face_coord_sub = self.create_subscription(Marker, '/people_marker', self.face_coord_callback, 10)
+
         # ROS2 publishers
         self.initial_pose_pub = self.create_publisher(PoseWithCovarianceStamped,
                                                       'initialpose',
                                                       10)
-        
+
         # ROS2 Action clients
         self.nav_to_pose_client = ActionClient(self, NavigateToPose, 'navigate_to_pose')
         self.spin_client = ActionClient(self, Spin, 'spin')
@@ -86,10 +98,56 @@ class RobotCommander(Node):
         self.dock_action_client = ActionClient(self, Dock, 'dock')
 
         self.get_logger().info(f"Robot commander has been initialized!")
-        
+
     def destroyNode(self):
         self.nav_to_pose_client.destroy()
-        super().destroy_node()     
+        super().destroy_node()
+
+    def face_coord_callback(self, msg):
+
+        self.face_coordinate_msg = msg
+        # Callback function to handel face coordinate messages
+        # self.get_logger().info(f"Received face marker: {msg}")
+
+        face_pose = msg.pose
+        face_position = face_pose.position
+
+        x = face_position.x
+        y = face_position.y
+        z = face_position.z
+
+        self.get_logger().info(f"Face coordinate: ({x}, {y}, {z})")
+        self.face_coordinate_received = True
+
+    def approachToFace(self, pose, behavior_tree=''):
+        """Send a `NavToPose` action request."""
+        self.debug("Waiting for 'NavigateToPose' action server")
+        while not self.nav_to_pose_client.wait_for_server(timeout_sec=1.0):
+            self.info("'NavigateToPose' action server not available, waiting...")
+
+        goal_msg = NavigateToPose.Goal()
+        goal_msg.pose = pose
+        goal_msg.behavior_tree = behavior_tree
+
+        self.info('Navigating to face: ' + str(pose.pose.position.x) + ' ' +
+                  str(pose.pose.position.y) + '...')
+        send_goal_future = self.nav_to_pose_client.send_goal_async(goal_msg,
+                                                                   self._feedbackCallback)
+        rclpy.spin_until_future_complete(self, send_goal_future)
+        self.goal_handle = send_goal_future.result()
+
+        if not self.goal_handle.accepted:
+            self.error('Face to ' + str(pose.pose.position.x) + ' ' +
+                       str(pose.pose.position.y) + ' was rejected!')
+            return False
+
+        while not self.isTaskComplete():
+            time.sleep(1)
+
+
+        time.sleep(2)
+        self.greeting()
+        return True
 
     def goToPose(self, pose, behavior_tree=''):
         """Send a `NavToPose` action request."""
@@ -116,6 +174,49 @@ class RobotCommander(Node):
         self.result_future = self.goal_handle.get_result_async()
         return True
 
+    def greeting(self):
+        text_to_speak = "Hello."
+        tts = gTTS(text=text_to_speak, lang='en')
+
+        temp_file = TemporaryFile()
+        tts.write_to_fp(temp_file)
+        temp_file.seek(0)
+
+        pygame.mixer.init()
+        pygame.mixer.music.load(temp_file)
+        pygame.mixer.music.play()
+
+        while pygame.mixer.music.get_busy():
+            time.sleep(0.1)
+
+        temp_file.close()
+
+    def move_robot_to_face(self, marker):
+        # Create a PoseStamped message for the face coordinate
+        pose = PoseStamped()
+        pose.header.frame_id = 'map'
+        pose.header.stamp = self.get_clock().now().to_msg()
+        pose.pose.position.x = marker.pose.position.x
+        pose.pose.position.y = marker.pose.position.y
+        pose.pose.position.z = marker.pose.position.z
+        pose.pose.orientation.w = 1.0  # Assuming no rotation is needed
+
+        self.info(f'Approach to Face - move robot{pose}')
+
+        if self.approachToFace(pose):
+            self.info("Robot reached the face coordinate")
+            #self.greeting()
+        else:
+            self.info("Fail to reach the face coordinate")
+
+        while not self.isTaskComplete():
+            self.info("Waiting to complete approach method")
+            time.sleep(1)
+
+        # Clear face coordinate message
+        self.face_coordinate_msg = None
+        self.face_coordinate_received = False
+
     def spin(self, spin_dist=1.57, time_allowance=10):
         self.debug("Waiting for 'Spin' action server")
         while not self.spin_client.wait_for_server(timeout_sec=1.0):
@@ -135,7 +236,7 @@ class RobotCommander(Node):
 
         self.result_future = self.goal_handle.get_result_async()
         return True
-    
+
     def undock(self):
         """Perform Undock action."""
         self.info('Undocking...')
@@ -250,7 +351,7 @@ class RobotCommander(Node):
                 self.debug(f'Result of get_state: {state}')
             time.sleep(2)
         return
-    
+
     def YawToQuaternion(self, angle_z = 0.):
         quat_tf = quaternion_from_euler(0, 0, angle_z)
 
@@ -268,7 +369,7 @@ class RobotCommander(Node):
         self.debug('Received action feedback message')
         self.feedback = msg.feedback
         return
-    
+
     def _dockCallback(self, msg: DockStatus):
         self.is_docked = msg.is_docked
 
@@ -296,9 +397,9 @@ class RobotCommander(Node):
     def debug(self, msg):
         self.get_logger().debug(msg)
         return
-    
+
 def main(args=None):
-    
+
     rclpy.init(args=args)
     rc = RobotCommander()
 
@@ -312,23 +413,61 @@ def main(args=None):
     # If it is docked, undock it first
     if rc.is_docked:
         rc.undock()
-    
+
     # Finally send it a goal to reach
-    goal_pose = PoseStamped()
-    goal_pose.header.frame_id = 'map'
-    goal_pose.header.stamp = rc.get_clock().now().to_msg()
 
-    goal_pose.pose.position.x = 2.6
-    goal_pose.pose.position.y = -1.3
-    goal_pose.pose.orientation = rc.YawToQuaternion(0.57)
+    goal_positions = [
+        #{'x': -1.0, 'y': -0.5, 'yaw': 0.57},
+        {'x': 0.0, 'y': -1.8, 'yaw': 0.57},
+        {'x': 2.6, 'y': -1.3, 'yaw': 0.57},
+        {'x': 1.0, 'y': -0.5, 'yaw': 1.0},
+        {'x': -1.0, 'y': 3.2, 'yaw': 0.57},
+        {'x': -1.5, 'y': 4.1, 'yaw': 0.57},
+        {'x': -1.0, 'y': 1.0, 'yaw': 0.57},
+        {'x': 1.0, 'y': 1.0, 'yaw': 0.57}
+    ]
 
-    rc.goToPose(goal_pose)
 
-    while not rc.isTaskComplete():
-        rc.info("Waiting for the task to complete...")
-        time.sleep(1)
+    for goal in goal_positions:
+        goal_pose = PoseStamped()
+        goal_pose.header.frame_id = 'map'
+        goal_pose.header.stamp = rc.get_clock().now().to_msg()
 
-    rc.spin(-0.57)
+        goal_pose.pose.position.x = goal['x']
+        goal_pose.pose.position.y = goal['y']
+        goal_pose.pose.orientation = rc.YawToQuaternion(goal['yaw'])
+
+        rc.goToPose(goal_pose)
+
+        while not rc.isTaskComplete():
+            # Check if face coordinate message is received
+            if rc.face_coordinate_received:
+                rc.info("face detected")
+
+                rc.detected_face_num = rc.detected_face_num + 1
+
+                # rc.cancelTask()
+                # Move robot to face coordinate
+                rc.move_robot_to_face(rc.face_coordinate_msg)
+
+                # Wait for 5 seconds
+                time.sleep(5)
+
+                #rc.greeting()
+                # Clear face coordinate message
+                rc.face_coordinate_msg = None
+                rc.face_coordinate_received = False
+
+                if(rc.detected_face_num > 2):
+                    break
+
+            else:
+                rc.info("Waiting")
+                time.sleep(1)
+        if(rc.detected_face_num > 2):
+            break
+        rc.info("Goal reached")
+
 
     rc.destroyNode()
 
